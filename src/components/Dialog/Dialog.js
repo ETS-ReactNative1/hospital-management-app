5682c1ec0136230a9e6608e6ab120b9647254cbb
ShogunRoss <nguyenconglong95@gmail.com>
2019-10-31 10:56:03 +0700

    * Change to function component

--- src/components/Dialog/Dialog.js
+++ src/components/Dialog/Dialog.js
@@ -1,84 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Modal, StatusBar } from 'react-native';
 import PropTypes from 'prop-types';
 import { Block, Card, Typography, Divider, GradientButton } from '..';
-import { theme } from 'src/constants';
+import { theme, localization } from 'src/constants';
 import styles from './DialogStyles';
+import AppData from 'src/AppData';
 
-export default class Dialog extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false
-    };
-  }
+const TextPackage = localization[AppData.language];
 
-  setVisible = boolean => {
-    this.setState({ visible: boolean });
-  };
+const Dialog = props => {
+  const {
+    title,
+    cancelText,
+    confirmText,
+    hideCancel,
+    confirmDisable,
+    handleConfirm,
+    handleCancel,
+    children,
+    visible,
+    ...other
+  } = props;
 
-  componentDidUpdate(prevProps) {
-    if (this.props.visible !== prevProps.visible) {
-      this.setVisible(this.props.visible);
-    }
-  }
-
-  render() {
-    const {
-      title,
-      cancelText,
-      confirmText,
-      hideCancel,
-      confirmDisable,
-      handleConfirm,
-      children
-    } = this.props;
-    const { visible } = this.state;
-    return (
-      <Modal
-        onRequestClose={() => {
-          !hideCancel && this.setVisible(!visible);
-        }}
-        animationType="slide"
-        transparent
-        visible={visible}
-      >
-        <StatusBar barStyle="default" backgroundColor={theme.colors.black2} />
-        <Block middle padding={[0, theme.sizes.base * 2]} backgroundColor={theme.colors.black2}>
-          <Card flex={false} shadow fullWidth>
-            {/* Title */}
-            <Typography bold h1>
-              {title}
-            </Typography>
-            <Divider />
-            {/* Content */}
-            {children}
-            {/* Action */}
-            <Block flex={false} row right>
-              {!hideCancel && (
-                <GradientButton onPress={() => this.setVisible(false)} style={styles.actionButton}>
-                  <Typography uppercase bold body color={theme.colors.gray}>
-                    {cancelText}
-                  </Typography>
-                </GradientButton>
-              )}
-
-              <GradientButton
-                onPress={handleConfirm}
-                disalbe={confirmDisable}
-                style={styles.actionButton}
-              >
-                <Typography uppercase bold body primary>
-                  {confirmText}
+  return (
+    <Modal
+      animationType="fade"
+      transparent
+      visible={visible}
+      onRequestClose={handleCancel}
+      {...other}
+    >
+      <StatusBar barStyle="default" backgroundColor={theme.colors.black2} />
+      <Block middle padding={[0, theme.sizes.base * 2]} backgroundColor={theme.colors.black2}>
+        <Card flex={false} shadow fullWidth>
+          {/* Title */}
+          <Typography bold h1>
+            {title}
+          </Typography>
+          <Divider />
+          {/* Content */}
+          {children}
+          {/* Action */}
+          <Block flex={false} row right>
+            {!hideCancel && (
+              <GradientButton onPress={handleCancel} style={styles.actionButton}>
+                <Typography uppercase bold body color={theme.colors.gray}>
+                  {cancelText}
                 </Typography>
               </GradientButton>
-            </Block>
-          </Card>
-        </Block>
-      </Modal>
-    );
-  }
-}
+            )}
+
+            <GradientButton
+              onPress={handleConfirm}
+              disable={confirmDisable}
+              style={styles.actionButton}
+            >
+              <Typography uppercase bold body primary>
+                {confirmText}
+              </Typography>
+            </GradientButton>
+          </Block>
+        </Card>
+      </Block>
+    </Modal>
+  );
+};
 
 Dialog.propTypes = {
   visible: PropTypes.bool.isRequired,
@@ -87,14 +73,18 @@ Dialog.propTypes = {
   confirmText: PropTypes.string,
   hideCancel: PropTypes.bool,
   confirmDisable: PropTypes.bool,
+  handleCancel: PropTypes.func,
   handleConfirm: PropTypes.func
 };
 
 Dialog.defaultProps = {
-  title: 'Tiêu đề',
-  cancelText: 'Hủy bỏ',
-  confirmText: 'Xác nhận',
+  title: TextPackage.TITLE,
+  cancelText: TextPackage.CANCEL,
+  confirmText: TextPackage.CONFIRM,
   hideCancel: false,
   confirmDisable: false,
+  handleCancel: () => console.log('cancle pressed'),
   handleConfirm: () => console.log('confirm pressed')
 };
+
+export default Dialog;
