d4829be3b67b4f5db50b65ca226e7a8b0369f33c
ShogunRoss <nguyenconglong95@gmail.com>
2019-11-08 22:02:29 +0700

    * change error popup

--- src/screens/App/Popup/ErrorPopup.js
+++ src/screens/App/Popup/ErrorPopup.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
 import { localization } from 'src/constants';
 import { popupActions } from 'src/redux/actions';
 import { connect } from 'react-redux';
 import AppData from 'src/AppData';
-import AppConst from 'src/AppConst';
+import { Typography, Dialog } from 'src/components';
 
 const TextPackage = localization[AppData.language];
 
-const ErrorHandle = errorMsg => {
+const handleError = errorMsg => {
   if (errorMsg.indexOf('Email is already taken') !== -1) {
     return {
       title: TextPackage.EMAIL_IS_TAKEN_ERR_TITLE,
       message: TextPackage.EMAIL_IS_TAKEN_ERR_MESSAGE,
-      okText: TextPackage.CONTINUE
+      confirmText: TextPackage.CONTINUE
     };
   }
 
@@ -21,7 +20,7 @@ const ErrorHandle = errorMsg => {
     return {
       title: TextPackage.NO_INTERNET_ERR_TITLE,
       message: TextPackage.NO_INTERNET_ERR_MESSAGE,
-      okText: TextPackage.CONTINUE
+      confirmText: TextPackage.CONTINUE
     };
   }
 
@@ -29,7 +28,7 @@ const ErrorHandle = errorMsg => {
     return {
       title: TextPackage.SIGN_IN_ERR,
       message: TextPackage.UNCONFIRMED_EMAIL_ERR,
-      okText: TextPackage.CONTINUE
+      confirmText: TextPackage.CONTINUE
     };
   }
 
@@ -37,29 +36,46 @@ const ErrorHandle = errorMsg => {
     return {
       title: TextPackage.SIGN_IN_ERR,
       message: TextPackage.INVALID_PASSWORD_ERR,
-      okText: TextPackage.CONTINUE
+      confirmText: TextPackage.CONTINUE
+    };
+  }
+
+  if (errorMsg.indexOf('Invalid old password') !== -1) {
+    return {
+      title: TextPackage.SIGN_IN_ERR,
+      message: TextPackage.INVALID_PASSWORD_ERR,
+      confirmText: TextPackage.CONTINUE
     };
   }
 
   return {
     title: TextPackage.UNDEFINED_ERR_TITLE,
     message: TextPackage.UNDEFINED_ERR_MESSAGE,
-    okText: TextPackage.CONTINUE
+    confirmText: TextPackage.CONTINUE
   };
 };
 
-const ErrorPopup = props => {
-  const { popup, showPopup } = props;
-  const { type, errorMsg } = popup;
-  if (type === AppConst.ERROR_POPUP) {
-    const errorPopupContent = { type: AppConst.OK_POPUP };
-    Object.assign(errorPopupContent, ErrorHandle(errorMsg));
-    showPopup(errorPopupContent);
-  }
-  return <View />;
+const ErrorPopup = ({ popupProps, hidePopup }) => {
+  const { message, ...otherProps } = handleError(popupProps.errorMsg);
+
+  return (
+    <Dialog hideCancel onRequestClose={hidePopup} {...otherProps}>
+      <Typography body justify>
+        {message}
+      </Typography>
+    </Dialog>
+  );
 };
 
+const mapStateToProps = state => state.popup;
+
+const mapDispatchToProps = dispatch => ({
+  hidePopup: () => {
+    dispatch(popupActions.hidePopup());
+  }
+});
+
 export default connect(
-  ({ popup }) => ({ popup }),
-  popupActions
+  mapStateToProps,
+  mapDispatchToProps
 )(ErrorPopup);
